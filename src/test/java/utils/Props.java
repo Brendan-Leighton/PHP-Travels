1f315e434e6fda15505426f5a87a6daa906e80c0
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-01 16:41:32 -0500

    Added utils for Props (.properties)

--- src/test/java/utils/Props.java
+++ src/test/java/utils/Props.java
@@ -1,2 +1,26 @@
-package utils;public class Props {
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Props {
+    Props props;
+    static Properties properties = new Properties();
+
+    private Props() {}
+
+    public static void init() {
+        try {
+            // C:\Users\br3nd\IdeaProjects\Selenium\src\test\java\resources\Props.properties
+            properties.load(new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\resources\\Props.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProp(String key) {
+        init();
+        return properties.getProperty(key);
+    }
 }
