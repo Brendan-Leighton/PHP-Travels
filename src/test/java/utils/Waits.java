1587efc5789a545c6920b4ccb17b4280eda167c5
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-01 16:42:07 -0500

    Added utils for custom WebDriverWaits

--- src/test/java/utils/Waits.java
+++ src/test/java/utils/Waits.java
@@ -1,6 +1,5 @@
 package utils;
 // SELENIUM
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
