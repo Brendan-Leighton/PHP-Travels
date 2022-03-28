61b8f47001343dff66c9e70d22c5870872955ee9
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-28 14:55:57 -0400

    initialized POM for home-page, no tests written yet.

--- src/test/java/tests/users/Users_Home.java
+++ src/test/java/tests/users/Users_Home.java
@@ -1,2 +1,27 @@
-package tests.users;public class Users_Home {
+package tests.users;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page_models._Factories;
+import page_models.users.Factory_Users_Home;
+import tests._BaseTest;
+import utils.Asserts;
+
+public class Users_Home extends _BaseTest {
+
+    Factory_Users_Home HOME;
+
+    public Users_Home() {
+        // PAGE FACTORIES
+        HOME = _Factories.getUsersHome();
+    }
+
+    @BeforeMethod
+    public void toHome() {
+        HOME.navigateTo();
+    }
+
+    @Test
+    public void PracticeTests() {
+    }
 }
