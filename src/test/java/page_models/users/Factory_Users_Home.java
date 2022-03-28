a1046fa455ecfb13bef4b0afacc4f2c901c196bb
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-28 14:58:34 -0400

    started POM factory

--- src/test/java/page_models/users/Factory_Users_Home.java
+++ src/test/java/page_models/users/Factory_Users_Home.java
@@ -1,2 +1,20 @@
-package page_models.users;public class Factory_Users_Home {
+package page_models.users;
+// SELENIUM
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Factory_Users_Home extends Factory__Index {
+
+    // CONSTRUCTOR
+    public Factory_Users_Home(WebDriver driver) {
+        super(
+                "https://www.phptravels.net/"
+        );
+        PageFactory.initElements(driver, this);
+    }
+
+
+
 }
