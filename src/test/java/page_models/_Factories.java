35e7cd7f3f4c12f4905a2d30122cb7584eb70641
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-28 15:01:08 -0400

    added home and navbar factories

--- src/test/java/page_models/_Factories.java
+++ src/test/java/page_models/_Factories.java
@@ -1,6 +1,8 @@
-package page_models.users;
+package page_models;
 
 import org.openqa.selenium.WebDriver;
+import page_models.users.Factory_Users_Home;
+import page_models.users.Factory_Users_Navbar;
 import utils.Drivers;
 
 /**
@@ -12,16 +14,36 @@ import utils.Drivers;
  */
 public class _Factories {
     private static _Factories FACTORIES;
-    Factory_Users_Home
 
-    private _Factories (WebDriver driver) {
-        this.home = new Factory_Home(driver);
+    // USERS PAGES
+    Factory_Users_Navbar navbar_users;
+    Factory_Users_Home home_users;
+
+    // CONSTRUCTOR
+    private _Factories(WebDriver driver) {
+        this.navbar_users = new Factory_Users_Navbar(driver);
+        this.home_users = new Factory_Users_Home(driver);
     }
 
+    // GETTER / INITIALIZER
     public static _Factories getFactories() {
         if (FACTORIES == null) {
             FACTORIES = new _Factories(Drivers.getDriver());
         }
         return FACTORIES;
+    }
+
+    //
+    // PAGE OBJECT GETTERS
+    //
+
+    // USERS
+    public static Factory_Users_Home getUsersHome() {
+        return getFactories().home_users;
+    }
+
+
+    public static Factory_Users_Navbar getUsersNavbar() {
+        return getFactories().navbar_users;
     }
 }
