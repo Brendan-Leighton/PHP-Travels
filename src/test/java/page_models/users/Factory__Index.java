2b94417e18194707f7d0829fe2c3a8bda9abe873
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-28 14:57:48 -0400

    after copy/pasted, made necesary changes

--- src/test/java/page_models/users/Factory__Index.java
+++ src/test/java/page_models/users/Factory__Index.java
@@ -1,14 +1,13 @@
-package page_models;
+package page_models.users;
 // JAVA
 import java.util.*;
 // SELENIUM
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+// TEST-NG
+import org.testng.asserts.SoftAssert;
 // MY CUSTOM
-import utils.Asserts;
-import utils.Drivers;
-import utils.Waits;
+import utils.*;
 
 /**
  * <h3>WebPageFactory METHODS:</h3>
@@ -22,5 +21,40 @@ import utils.Waits;
  * </ul>
  */
 public abstract class Factory__Index {
+    // PROPS
+    private final String pageUrl;
+    private final String pageHomeUrl = SiteUrls.getHome();;
+    private final List<WebElement> navigationElements = new ArrayList<>();
 
+    // CONSTRUCTOR
+    public Factory__Index(String webPageUrl) {
+        pageUrl = webPageUrl;
+//        spreadsheet_data = pages.BaseTest.getSpreadsheet_Data();
+    }
+
+
+
+    /**
+     * <h1>Navigate to this page</h1>
+     * <ol>
+     *     <li>Check if we're already on this webpage</li>
+     *     <li>IF: Not on this page, go to website's home-page</li>
+     *     iteration not yet added
+     *     <li>Iterate a List of WebElements to traverse the site until we get to this page</li>
+     * </ol>
+     */
+    public void navigateTo() {
+        if (Drivers.getDriver().getCurrentUrl().equals(this.pageHomeUrl)) {
+            return;
+        }
+
+        // navigate to home page
+        Drivers.getDriver().navigate().to(this.pageHomeUrl);
+        // wait for page load and assert correct url
+        Asserts.navigation_toUrl(this.pageHomeUrl);
+    }
+
+    public String getUrl() {
+        return this.pageUrl;
+    }
 }
