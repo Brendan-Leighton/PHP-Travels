786e131e0802c35dfc0add8516d93ce56023887f
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-03-29 16:10:22 -0400

    working on accessing navbar items inside a dropdown, creating a method to handle it

--- src/test/java/page_models/users/Factory_Users_Navbar.java
+++ src/test/java/page_models/users/Factory_Users_Navbar.java
@@ -1,21 +1,33 @@
 package page_models.users;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import utils.Drivers;
 import utils.SiteUrls;
+import utils.Waits;
 
 public class Factory_Users_Navbar extends Factory__Index {
+
+
+    Actions actionProvider;
     // CONSTRUCTOR
     public Factory_Users_Navbar(WebDriver driver) {
         super(SiteUrls.getHome());
         PageFactory.initElements(driver, this);
+
+        actionProvider = new Actions(driver);
     }
 
+    //
     // NAVBAR BUTTONS - LOCATORS
+    //
+
     @FindBy(xpath = "//div[@class=\"logo\"]/a")
     private WebElement navbar_logo;
 
@@ -43,7 +55,32 @@ public class Factory_Users_Navbar extends Factory__Index {
     @FindBy (xpath = "//nav/ul/li/a[contains(text(), \"Company\")]")
     private WebElement navbar_company;
 
+    // company dropdown
+
+    @FindBy (xpath = "//nav/ul/li")
+    private WebElement navbar_company_dropdown;
+
+    public WebElement getNavbar_company_dropdown() {
+        return navbar_company_dropdown;
+    }
+
+    private final String dropdownEl = "//div[@class=\"main-menu-content\"]/nav/ul//li[@class=\"footm\"]/ul[@class=\"dropdown-menu-item\"]";
+
+    @FindBy (xpath = dropdownEl + "/li/a[contains(text(), \"About Us\")]")
+    private WebElement navbar_dropdown_aboutUs;
+
+    @FindBy (xpath = dropdownEl + "/li/a[contains(text(), \"Terms of Use\")]")
+    private WebElement navbar_dropdown_termsOfUse;
+
+    @FindBy (xpath = dropdownEl + "/li/a[contains(text(), \"FAQ\")]")
+    private WebElement navbar_dropdown_faq;
+
+    @FindBy (xpath = dropdownEl + "/li/a[contains(text(), \"How to Book\")]")
+    private WebElement navbar_dropdown_howToBook;
+
+    //
     // NAVBAR BUTTONS - GETTERS
+    //
     public WebElement getNavbar_logo() {return navbar_logo;}
     public WebElement getNavbar_home() {return navbar_home;}
     public WebElement getNavbar_hotels() {return navbar_hotels;}
@@ -53,6 +90,10 @@ public class Factory_Users_Navbar extends Factory__Index {
     public WebElement getNavbar_blog() {return navbar_blog;}
     public WebElement getNavbar_offers() {return navbar_offers;}
     public WebElement getNavbar_company() {return navbar_company;}
+    public WebElement getNavbar_dropdown_aboutUs() {return navbar_dropdown_aboutUs;}
+    public WebElement getNavbar_dropdown_termsOfUse() {return navbar_dropdown_termsOfUse;}
+    public WebElement getNavbar_dropdown_faq() {return navbar_dropdown_faq;}
+    public WebElement getNavbar_dropdown_howToBook() {return navbar_dropdown_howToBook;}
 
     /**
      * Test navbar navigation.
@@ -69,6 +110,41 @@ public class Factory_Users_Navbar extends Factory__Index {
     public void test(String fromUrl, WebElement navEl, String toUrl) {
         Drivers.getDriver().navigate().to(fromUrl);
         navEl.click();
+        Assert.assertEquals(Drivers.getDriver().getCurrentUrl(), toUrl);
+    }
+
+    /**
+     * Test navbar navigation where navbar element has a dropdown
+     * <ol>
+     *     <li>Navigate to the "fromUrl"</li>
+     *     <li>Move mouse to the "dropdownEl" which is nav element with a dropdown</li>
+     *     <li>Click on the "navEl" in the dropdown</li>
+     *     <li>Assert arrival to the "toUrl"</li>
+     * </ol>
+     *
+     * @param fromUrl : The URL of the page whose navbar we are testing
+     * @param navEl : The WebElement in the navbar we are testing
+     * @param toUrl : The Expected destination after click the navbar element
+     */
+    public void testDropDownLink(String fromUrl, WebElement dropdownEl, WebElement navEl, String toUrl) {
+
+        // navigate to page with navbar being tested
+        Drivers.getDriver().navigate().to(fromUrl);
+
+        // check for that damn cookie popup message
+        try {
+            WebElement damnCookiePopup = Drivers.getDriver().findElement(new By.ById("cookie_stop"));
+            Waits.forElement(damnCookiePopup);
+            damnCookiePopup.click();
+        } catch (Exception ignore) {}
+
+        // Performs mouse move action onto the element with a dropdown
+        new Actions(Drivers.getDriver()).moveToElement(dropdownEl).perform();
+
+        // click that bad boiiii
+        Waits.forElement_andClick(navEl);
+
+        // assert proper url after navigation
         Assert.assertEquals(Drivers.getDriver().getCurrentUrl(), toUrl);
     }
 }
